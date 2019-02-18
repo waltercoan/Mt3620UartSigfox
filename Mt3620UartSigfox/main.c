2b9c739da1f8a7d60908de77d61a57822e5e4468
Walter Silvestre Coan <walter.coan@gmail.com>
2019-02-18 18:34:58 -0300

    first

---
+++ Mt3620UartSigfox/main.c
@@ -0,0 +1,243 @@
+﻿#include <errno.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+// applibs_versions.h defines the API struct versions to use for applibs APIs.
+#include "applibs_versions.h"
+#include "epoll_timerfd_utilities.h"
+#include <applibs/uart.h>
+#include <applibs/gpio.h>
+#include <applibs/log.h>
+
+#include "mt3620_rdb.h"
+
+// This sample C application for the MT3620 Reference Development Board (Azure Sphere)
+// demonstrates how to use a UART (serial port).
+// The sample opens a UART with a baud rate of 115200. Pressing a button causes characters
+// to be sent from the device over the UART; data received by the device from the UART is echoed to
+// the Visual Studio Output Window, and causes an LED to blink.
+//
+// It uses the API for the following Azure Sphere application libraries:
+// - UART (serial port)
+// - GPIO (digital input for button)
+// - log (messages shown in Visual Studio's Device Output window during debugging)
+
+// File descriptors - initialized to invalid value
+static int uartFd = -1;
+static int triggerSendButtonGpioFd = -1;
+static int buttonPollTimerFd = -1;
+static int incomingDataLedGpioFd = -1;
+static int epollFd = -1;
+
+// State variables
+static GPIO_Value_Type buttonState = GPIO_Value_High;
+static size_t totalBytesReceived = 0;
+
+// Termination state
+static volatile sig_atomic_t terminationRequired = false;
+
+/// <summary>
+///     Signal handler for termination requests. This handler must be async-signal-safe.
+/// </summary>
+static void TerminationHandler(int signalNumber)
+{
+    // Don't use Log_Debug here, as it is not guaranteed to be async-signal-safe.
+    terminationRequired = true;
+}
+
+/// <summary>
+///     Helper function to send a fixed message via the given UART.
+/// </summary>
+/// <param name="uartFd">The open file descriptor of the UART to write to</param>
+/// <param name="dataToSend">The data to send over the UART</param>
+static void SendUartMessage(int uartFd, const char *dataToSend)
+{
+    size_t totalBytesSent = 0;
+    size_t totalBytesToSend = strlen(dataToSend);
+    int sendIterations = 0;
+    while (totalBytesSent < totalBytesToSend) {
+        sendIterations++;
+
+        // Send as much of the remaining data as possible
+        size_t bytesLeftToSend = totalBytesToSend - totalBytesSent;
+        const char *remainingMessageToSend = dataToSend + totalBytesSent;
+        ssize_t bytesSent = write(uartFd, remainingMessageToSend, bytesLeftToSend);
+        if (bytesSent < 0) {
+            Log_Debug("ERROR: Could not write to UART: %s (%d).\n", strerror(errno), errno);
+            terminationRequired = true;
+            return;
+        }
+
+        totalBytesSent += (size_t)bytesSent;
+    }
+
+    Log_Debug("Sent %zu bytes over UART in %d calls.\n", totalBytesSent, sendIterations);
+}
+
+/// <summary>
+///     Handle button timer event: if the button is pressed, send data over the UART.
+/// </summary>
+static void ButtonPollTimerEventHandler(EventData *eventData)
+{
+    if (ConsumeTimerFdEvent(buttonPollTimerFd) != 0) {
+        terminationRequired = true;
+        return;
+    }
+
+    // Check for a button press
+    GPIO_Value_Type newButtonState;
+    int result = GPIO_GetValue(triggerSendButtonGpioFd, &newButtonState);
+    if (result != 0) {
+        Log_Debug("ERROR: Could not read button GPIO: %s (%d).\n", strerror(errno), errno);
+        terminationRequired = true;
+        return;
+    }
+
+    // If the button has just been pressed, send data over the UART
+    // The button has GPIO_Value_Low when pressed and GPIO_Value_High when released
+    if (newButtonState != buttonState) {
+        if (newButtonState == GPIO_Value_Low) {
+            const char *messageToSend = "AT$RC\n\rAT$SF=692665535048455245\n\r";
+            SendUartMessage(uartFd, messageToSend);
+        }
+        buttonState = newButtonState;
+    }
+}
+
+/// <summary>
+///     Handle UART event: if there is incoming data, print it, and blink the LED.
+/// </summary>
+static void UartEventHandler(EventData *eventData)
+{
+    const size_t receiveBufferSize = 256;
+    uint8_t receiveBuffer[receiveBufferSize + 1]; // allow extra byte for string termination
+    ssize_t bytesRead;
+
+    // Read UART message
+    bytesRead = read(uartFd, receiveBuffer, receiveBufferSize);
+    if (bytesRead < 0) {
+        Log_Debug("ERROR: Could not read UART: %s (%d).\n", strerror(errno), errno);
+        terminationRequired = true;
+        return;
+    }
+
+    if (bytesRead > 0) {
+        // Null terminate the buffer to make it a valid string, and print it
+        receiveBuffer[bytesRead] = 0;
+        Log_Debug("UART received %d bytes: '%s'.\n", bytesRead, (char *)receiveBuffer);
+
+        // If the total received bytes is odd, turn the LED on, otherwise turn it off
+        totalBytesReceived += (size_t)bytesRead;
+        int result =
+            GPIO_SetValue(incomingDataLedGpioFd,
+                          (totalBytesReceived % 2) == 1 ? GPIO_Value_Low : GPIO_Value_High);
+        if (result != 0) {
+            Log_Debug("ERROR: Could not set LED output value: %s (%d).\n", strerror(errno), errno);
+            terminationRequired = true;
+        }
+    }
+}
+
+// event handler data structures. Only the event handler field needs to be populated.
+static EventData buttonPollTimerEventData = {.eventHandler = &ButtonPollTimerEventHandler};
+static EventData uartEventData = {.eventHandler = &UartEventHandler};
+
+/// <summary>
+///     Set up SIGTERM termination handler, initialize peripherals, and set up event handlers.
+/// </summary>
+/// <returns>0 on success, or -1 on failure</returns>
+static int InitPeripheralsAndHandlers(void)
+{
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = TerminationHandler;
+    sigaction(SIGTERM, &action, NULL);
+
+    epollFd = CreateEpollFd();
+    if (epollFd < 0) {
+        return -1;
+    }
+
+    // Create a UART_Config object, open the UART and set up UART event handler
+    UART_Config uartConfig;
+    UART_InitConfig(&uartConfig);
+    uartConfig.baudRate = 9600;
+    uartConfig.flowControl = UART_FlowControl_None;
+    uartFd = UART_Open(MT3620_RDB_HEADER2_ISU0_UART, &uartConfig);
+    if (uartFd < 0) {
+        Log_Debug("ERROR: Could not open UART: %s (%d).\n", strerror(errno), errno);
+        return -1;
+    }
+    if (RegisterEventHandlerToEpoll(epollFd, uartFd, &uartEventData, EPOLLIN) != 0) {
+        return -1;
+    }
+
+    // Open button GPIO as input, and set up a timer to poll it
+    Log_Debug("Opening MT3620_RDB_BUTTON_A as input.\n");
+    triggerSendButtonGpioFd = GPIO_OpenAsInput(MT3620_RDB_BUTTON_A);
+    if (triggerSendButtonGpioFd < 0) {
+        Log_Debug("ERROR: Could not open button GPIO: %s (%d).\n", strerror(errno), errno);
+        return -1;
+    }
+    struct timespec buttonPressCheckPeriod = {0, 1000000};
+    buttonPollTimerFd = CreateTimerFdAndAddToEpoll(epollFd, &buttonPressCheckPeriod,
+                                                   &buttonPollTimerEventData, EPOLLIN);
+    if (buttonPollTimerFd < 0) {
+        return -1;
+    }
+
+    // Open LED GPIO and set as output with value GPIO_Value_High (off)
+    Log_Debug("Opening MT3620_RDB_LED2_RED.\n");
+    incomingDataLedGpioFd =
+        GPIO_OpenAsOutput(MT3620_RDB_LED2_RED, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    if (incomingDataLedGpioFd < 0) {
+        Log_Debug("ERROR: Could not open LED GPIO: %s (%d).\n", strerror(errno), errno);
+        return -1;
+    }
+
+    return 0;
+}
+
+/// <summary>
+///     Close peripherals and handlers.
+/// </summary>
+static void ClosePeripheralsAndHandlers(void)
+{
+    // Leave the LED off
+    if (incomingDataLedGpioFd >= 0) {
+        GPIO_SetValue(incomingDataLedGpioFd, GPIO_Value_High);
+    }
+
+    Log_Debug("Closing file descriptors.\n");
+    CloseFdAndPrintError(incomingDataLedGpioFd, "IncomingDataLedGpio");
+    CloseFdAndPrintError(buttonPollTimerFd, "ButtonPollTimer");
+    CloseFdAndPrintError(triggerSendButtonGpioFd, "TriggerSendButtonGpio");
+    CloseFdAndPrintError(uartFd, "Uart");
+    CloseFdAndPrintError(epollFd, "Epoll");
+}
+
+/// <summary>
+///     Main entry point for this application.
+/// </summary>
+int main(int argc, char *argv[])
+{
+    Log_Debug("UART application starting.\n");
+    if (InitPeripheralsAndHandlers() != 0) {
+        terminationRequired = true;
+    }
+
+    // Use epoll to wait for events and trigger handlers, until an error or SIGTERM happens
+    while (!terminationRequired) {
+        if (WaitForEventAndCallHandler(epollFd) != 0) {
+            terminationRequired = true;
+        }
+    }
+
+    ClosePeripheralsAndHandlers();
+    Log_Debug("Application exiting.\n");
+    return 0;
+}
